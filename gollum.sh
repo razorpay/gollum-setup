ffd33b18b15c1bf995986e924e88236f3be70173
Nemo <me@captnemo.in>
2017-12-09 14:27:20 +0530

    Setup git at boot

--- gollum.sh
+++ gollum.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/dumb-init /bin/sh
 cd /app
+
+if [ ! -d /wiki/.git ]; then
+  git init /wiki
+fi
+
 oauth2_proxy -client-id "$CLIENT_ID" -client-secret "$CLIENT_SECRET" \
 		-email-domain "$EMAIL_DOMAIN" -http-address "0.0.0.0:80" \
 		-cookie-secret "$COOKIE_SECRET" -redirect-url "$REDIRECT_URL" \
